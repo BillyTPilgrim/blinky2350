9cd90fa51d7ba560fee921778cfbee37545e8096
Phil Howard <github@gadgetoid.com>
2025-09-22 16:29:28 +0100

    Switch over to app model.

--- firmware/launcher.py
+++ firmware/launcher.py
@@ -9,24 +9,75 @@ import blinky_os
 
 display = blinky2350.Blinky2350()
 
-ICONS = {
-    "badge": "\uea67",
-    "book_2": "\uf53e",
-    "check_box": "\ue834",
-    "cloud": "\ue2bd",
-    "deployed-code": "\uf720",
-    "description": "\ue873",
-    "help": "\ue887",
-    "water_full": "\uf6d6",
-    "wifi": "\ue63e",
-    "image": "\ue3f4",
-    "info": "\ue88e",
-    "format_list_bulleted": "\ue241",
-    "joystick": "\uf5ee",
-    "playing_cards": "\uf5dc"
-}
 
-APP_DIR = "/examples"
+class App:
+    ICONS = {
+        "badge": "\uea67",
+        "book_2": "\uf53e",
+        "check_box": "\ue834",
+        "cloud": "\ue2bd",
+        "deployed-code": "\uf720",
+        "description": "\ue873",
+        "help": "\ue887",
+        "water_full": "\uf6d6",
+        "wifi": "\ue63e",
+        "image": "\ue3f4",
+        "info": "\ue88e",
+        "format_list_bulleted": "\ue241",
+        "joystick": "\uf5ee",
+        "playing_cards": "\uf5dc"
+    }
+    DIRECTORY = "apps"
+    DEFAULT_ICON = "description"
+    ERROR_ICON = "help"  # TODO: have a reserved error icon
+
+    def __init__(self, name):
+        self._file = name
+        self._meta = {
+            "NAME": name,
+            "ICON": App.DEFAULT_ICON,
+            "DESC": ""
+        }
+        self.path = f"{name}/__main__"
+        self._loaded = False
+
+    def read_metadata(self):
+        if self._loaded:
+            return
+
+        try:
+            exec(open(f"{App.DIRECTORY}/{self._file}/__init__.py", "r").read(), self._meta)
+        except SyntaxError:
+            self._meta["ICON"] = App.ERROR_ICON
+        self._loaded = True
+
+    @property
+    def name(self):
+        self.read_metadata()
+        return self._meta["NAME"]
+
+    @property
+    def icon(self):
+        self.read_metadata()
+        try:
+            return App.ICONS[self._meta["ICON"]]
+        except KeyError:
+            return App.ICONS[App.ERROR_ICON]
+
+    @property
+    def desc(self):
+        self.read_metadata()
+        return self._meta["DESC"]
+
+    @staticmethod
+    def is_valid(file):
+        try:
+            open(f"{App.DIRECTORY}/{file}/__init__.py", "r")
+            return True
+        except OSError:
+            return False
+
+
 FONT_SIZE = 1
 
 changed = False
@@ -56,11 +107,11 @@ vector.set_font("Roboto-Medium-With-Material-Symbols.af", 20)
 vector.set_font_align(HALIGN_CENTER)
 vector.set_transform(t)
 
-examples = [x[:-3] for x in os.listdir(APP_DIR) if x.endswith(".py")]
+apps = [App(x) for x in os.listdir(App.DIRECTORY) if App.is_valid(x)]
 
 MAX_PER_ROW = 1
 MAX_PER_PAGE = 1
-ICONS_TOTAL = len(examples)
+ICONS_TOTAL = len(apps)
 MAX_PAGE = ICONS_TOTAL
 
 WIDTH, HEIGHT = display.get_bounds()
@@ -74,38 +125,9 @@ CENTRE_X = WIDTH // 2
 CENTRE_Y = HEIGHT // 2
 
 
-def read_header(label):
-    file = f"{APP_DIR}/{label}.py"
-
-    name = label
-    icon = ICONS["description"]
-
-    with open(file) as f:
-        header = [f.readline().strip() for _ in range(3)]
-
-    for line in header:
-        if line.startswith("# ICON "):
-            icon = line[7:].strip()
-            icon = ICONS[icon]
-
-        if line.startswith("# NAME "):
-            name = line[7:]
-
-    return name, icon
-
-
-def draw_icon(icon, x, y):
-
-    display.set_pen(FOREGROUND)
-    vector.set_font_size(10)
-    vector.set_transform(t)
-    vector.text(icon, x, y)
-
-
 def launch_transition():
-
     x, y = CENTRES
-    _, icon = read_header(examples[selected_index])
+    app = apps[selected_index]
 
     for i in range(25):
         display.set_pen(BACKGROUND)
@@ -114,7 +136,7 @@ def launch_transition():
         display.set_pen(FOREGROUND)
         vector.set_font_size(10 + i)
         vector.set_transform(t)
-        vector.text(icon, x, y)
+        vector.text(app.icon, x, y)
 
         t.reset()
         display.update()
@@ -125,50 +147,51 @@ def launch_transition():
     display.update()
 
 
-def render(selected_index):
-    global transition
+transition_start_time = 0
 
-    # Bouncy bouncy icons
-    ticks = time.ticks_ms() / 1000.0
-    bounce = int(math.sin(ticks * 12) * 2)
+def render(selected_index, previous_index, transition_start):
+    global transition_start_time
+
+    TRANSITION_TIME = 100
+    TRANSITION_DISTANCE = 40
+    TRANSITION_OUT_SPEED = 1.4
 
     display.set_pen(BACKGROUND)
     display.clear()
+    display.set_pen(FOREGROUND)
+    vector.set_font_size(10)
+    vector.set_transform(t)
+
+    if transition_start:
+        transition_start_time = time.ticks_ms()
+
+    # Bouncy bouncy icons
+    ticks = time.ticks_ms()
+    bounce = int(math.sin(ticks / 1000.0 * 12) * 2)
 
     # get the names of the previous and current apps
-    icon_current = examples[selected_index]
-    icon_previous = examples[previous_index]
+    app_current = apps[selected_index]
+    app_previous = apps[previous_index]
 
     x, y = CENTRES
     y += bounce
 
-    _, icon_current = read_header(icon_current)
-
     # transition from one icon to the next over a period of 100ms
-    if transition:
-        _, icon_prev = read_header(icon_previous)
-        direction = selected_index > previous_index
+    direction = selected_index > previous_index
 
-        for i in range(20):
-            display.set_pen(BACKGROUND)
-            display.clear()
-            display.set_pen(FOREGROUND)
+    i = time.ticks_diff(ticks, transition_start_time)
+    i = int(min(TRANSITION_TIME, i) / TRANSITION_TIME * TRANSITION_DISTANCE)
 
-            offset = i * 2
-            prev_x = x + offset if direction else x - offset
-            curr_x = i if direction else WIDTH - i
+    curr_x = x - (TRANSITION_DISTANCE - i) if direction else x + (TRANSITION_DISTANCE - i)
 
-            vector.text(icon_prev, prev_x, y)
-            vector.text(icon_current, curr_x, y)
+    i *= TRANSITION_OUT_SPEED
+    i = int(i)
+    prev_x = x + i if direction else x - i
 
-            display.update()
-            time.sleep(0.005)
+    vector.text(app_previous.icon, prev_x, y)
+    vector.text(app_current.icon, curr_x, y)
 
-        # Set transition to false once the animation has finished.
-        transition = False
-    else:
-        draw_icon(icon_current, x, y)
-        display.update()
+    display.update()
 
     gc.collect()
 
@@ -179,12 +202,13 @@ def wait_for_user_to_release_buttons():
 
 
 def launch_example(file):
-    wait_for_user_to_release_buttons()
+    # ???
+    # wait_for_user_to_release_buttons()
     launch_transition()
 
     time.sleep(1)
 
-    file = f"{APP_DIR}/{file}"
+    file = f"{App.DIRECTORY}/{file}"
 
     for k in locals().keys():
         if k not in ("gc", "file", "blinky_os"):
@@ -195,37 +219,39 @@ def launch_example(file):
     blinky_os.launch(file)
 
 
-try:
-    selected_index = examples.index(state["selected_file"])
-except (ValueError, KeyError):
-    selected_index = 0
+def app_index(file):
+    index = 0
+    for app in apps:
+        if app.path == file:
+            return index
+        index += 1
+    return 0
 
+
+selected_index = app_index(state["selected_file"])
 previous_index = 0
+
 
 while True:
 
     if display.pressed(blinky2350.BUTTON_B):
         launch_example(state["selected_file"])
 
-    if display.pressed(blinky2350.BUTTON_C):
-        if selected_index >= MAX_PER_PAGE:
-            previous_index = selected_index
-            selected_index -= MAX_PER_PAGE
-            transition = True
-            changed = True
+    if display.pressed(blinky2350.BUTTON_C) and selected_index > 0:
+        previous_index = selected_index
+        selected_index -= 1
+        changed = True
+        print(apps[selected_index].name)
 
-    if display.pressed(blinky2350.BUTTON_A):
-        if selected_index < ICONS_TOTAL - 1:
-            previous_index = selected_index
-            selected_index += MAX_PER_PAGE
-            selected_index = min(selected_index, ICONS_TOTAL - 1)
-            transition = True
-            changed = True
+    if display.pressed(blinky2350.BUTTON_A) and selected_index < ICONS_TOTAL - 1:
+        previous_index = selected_index
+        selected_index += 1
+        changed = True
+        print(apps[selected_index].name)
 
     if changed:
-        state["selected_file"] = examples[selected_index]
+        state["selected_file"] = apps[selected_index].path
         blinky_os.state_save("launcher", state)
-        changed = False
-        # wait_for_user_to_release_buttons()
 
-    render(selected_index)
+    render(selected_index, previous_index, changed)
+    changed = False
